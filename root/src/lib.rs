30777753589f5c87d49b396be94918da3fd04b28
wmcgee3 <william.j.mcgee3@gmail.com>
2022-11-04 13:52:03 -0400

    add things route to root links

--- root/src/lib.rs
+++ root/src/lib.rs
@@ -23,8 +23,11 @@ impl RootResponse {
 /// A simple Spin HTTP component.
 #[http_component]
 fn hello_spin(_: Request) -> Result<Response> {
-    let response_body =
-        RootResponse::new(&vec![Link::new("self", "/"), Link::new("stuff", "/stuff")]);
+    let response_body = RootResponse::new(&vec![
+        Link::new("self", "/"),
+        Link::new("stuff", "/stuff"),
+        Link::new("things", "/things"),
+    ]);
     Ok(http::Response::builder()
         .status(200)
         .header("content-type", "application/json")
